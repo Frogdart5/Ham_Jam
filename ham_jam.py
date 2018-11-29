ce4bbcaf87cbf92bc204bf10025ae6255f269a5c
John Engh <14243398+Frogdart5@users.noreply.github.com>
2018-11-28 19:52:10 -0600

    Added attack command

--- ham_jam.py
+++ ham_jam.py
@@ -80,6 +80,9 @@ def load_dialogue(lang="en_us"):
 def get_items():
     item_name = []
     item_effect = []
+    item_strength = []
+    item_display_key = []
+    item_rarity = []
     items_file = open("Assets/items.csv", "r")
     items_str = items_file.read().split("\n")
     for line in items_str:
@@ -87,8 +90,9 @@ def get_items():
             item_list = line.split(",")
             item_name.append(item_list[0])
             item_effect.append(item_list[1])
-
-    # TODO: get items
+            item_strength.append(item_list[2])
+            item_display_key.append(item_list[3])
+            item_rarity.append(item_list[4])
     return
 
 
@@ -119,54 +123,54 @@ def load_menu():
 
 def create_room(x, y):
     global world_enemies
-    global world_rooms
     spawning_chance = random.randint(0, 100)
+    print(dialogue['debug.createRoom'])
     if spawning_chance >= 75:  # Spawns item if chance over 75%
         print("spawned item:", end=' ')
         spawned_entity = 1
         entity_uuid = uuid.uuid4()
         item_spawn_chance = random.randint(0, 100)
-        if item_spawn_chance <= 75:
-            print("common")
-            item_type = 'common'
-        elif item_spawn_chance <= 90:
-            print("uncommon")
-            item_type = 'uncommon'
-        elif item_spawn_chance <= 99:
-            print("rare")
-            item_type = 'rare'
-        elif item_spawn_chance <= 100:
+        if item_spawn_chance == 100:
             print("legendary")
             item_type = 'legendary'
+        elif item_spawn_chance >= 99:
+            print("rare")
+            item_type = 'rare'
+        elif item_spawn_chance >= 90:
+            print("uncommon")
+            item_type = 'uncommon'
+        else:
+            print("common")
+            item_type = 'common'
         world_items[entity_uuid] = [item_type]
     elif spawning_chance >= 50:  # spawn enemy
         print("spawned enemy:", end=' ')
         spawned_entity = 2
         entity_uuid = uuid.uuid4()
         enemy_spawn_chance = random.randint(0, 100)
-        if enemy_spawn_chance <= 75:
-            print("common")
-            enemy_type = 'common'
-            health = 10
-            attack = 10
-        elif enemy_spawn_chance <= 90:
-            print("uncommon")
-            enemy_type = 'uncommon'
-            health = 10
-            attack = 10
-        elif enemy_spawn_chance <= 99:
-            print("rare")
-            enemy_type = 'rare'
-            health = 10
-            attack = 10
-        elif enemy_spawn_chance <= 100:
+        if enemy_spawn_chance == 100:
             print("legendary")
             enemy_type = 'legendary'
             health = 10
-            attack = 10
-        world_enemies[entity_uuid] = [enemy_type, health, attack]
+            enemy_attack = 10
+        elif enemy_spawn_chance >= 99:
+            print("rare")
+            enemy_type = 'rare'
+            health = 10
+            enemy_attack = 5
+        elif enemy_spawn_chance >= 90:
+            print("uncommon")
+            enemy_type = 'uncommon'
+            health = 10
+            enemy_attack = 3
+        else:
+            print("common")
+            enemy_type = 'common'
+            health = 10
+            enemy_attack = 2
+        world_enemies[entity_uuid] = [enemy_type, health, enemy_attack]
     else:
-        print("nothing spawned")
+        print(dialogue['debug.noSpawn'])
         spawned_entity = 0
         entity_uuid = ''
     if x not in rooms:
@@ -182,9 +186,14 @@ def load_room(x, y):
             room_exists = True
     if not room_exists:
         create_room(x, y)
+
+    print(dialogue['debug.loadRoom'])
+    if rooms[x][y][0] == 0:
+        print(dialogue['walk.empty'])
     else:
-        print("Loading room")
-        # TODO: Load Room
+        entity_uuid = rooms[x][y][1]
+        entity_name = entity_uuid  # TODO: find name of entity
+        print(dialogue['walk.occupied'] % entity_name)
     return
 
 
@@ -194,72 +203,117 @@ def game_exit():  # exit the game and finish the process
     return
 
 
-def walk(direction):
+def walk(direction=''):
     global x_pos
     global y_pos
     moved = False
     if direction.lower() == dialogue['walk.north']:
-        print("walking North")
         x_pos += 1
         moved = True
     elif direction.lower() == dialogue['walk.south']:
-        print("walking Sorth")
         x_pos += -1
         moved = True
     elif direction.lower() == dialogue['walk.east']:
-        print("walking East")
         y_pos += 1
         moved = True
     elif direction.lower() == dialogue['walk.west']:
-        print("walking West")
         y_pos += -1
         moved = True
     if moved:
+        print(dialogue['walk.walking'] % direction.title())
         load_room(x_pos, y_pos)
-        print("X:", x_pos, "Y:", y_pos)
+        print("X: %d Y: %d" % (x_pos, y_pos))
+    elif direction == '':
+        print(dialogue['walk.noArgs'])
     else:
-        print("invalid direction")
+        print(dialogue['walk.unknown'])
     return
 
 
 def game_quit():
     # save game
     load_menu()
-
     return
 
 
 def game_start():
-    # TODO: start game
-    global x_pos
-    global y_pos
-    global rooms
-    global world_enemies
-    global world_items
-    world_items = {}
-    x_pos = 0
-    y_pos = 0
-    rooms = {0: {0: [0, '']}}
-    world_enemies = {}
-    char_health = 20
-
-    print("starting game")
+    # TODO: Load save file for game
+    if debug_immortal:
+        print(dialogue['debug.optionsEnabled'])
+    print(dialogue['debug.start'])
     set_avail_cmd("game.main")  # Sets available commands to those used during gameplay
-    # load first room
-    # load room
+    load_room(x_pos, y_pos)
     return
 
 
 def options():
     print(dialogue['menu.options'])
+    set_avail_cmd("menu.options")
     return
 
 
-all_cmd = {'walk': walk, 'quit': game_quit, 'start': game_start, 'exit': game_exit, 'options': options}
+def attack(weapon="hand"):
+    global rooms
+    global x_pos
+    global y_pos
+    global inventory
+    global world_enemies
+    global char_health
+    global char_defense
+    global debug_immortal
+    if rooms[x_pos][y_pos][0] == 2:
+        if weapon in inventory or weapon == 'hand':
+            print("attacking with %s" % weapon)
+            enemy_uuid = rooms[x_pos][y_pos][1]
+            enemy_health = world_enemies[enemy_uuid][1]
+            enemy_strength = world_enemies[enemy_uuid][2]
+            enemy_name = enemy_uuid
+            if weapon == 'hand':
+                weapon_strength = 1  # TODO: get weapon strength
+            else:
+                weapon_strength = 5
+            enemy_health -= weapon_strength
+            world_enemies[enemy_uuid][1] = enemy_health
+            print(dialogue['debug.attack'] % (weapon_strength, enemy_name, enemy_health))
+            if enemy_health <= 0:  # remove Enemy
+                print(dialogue['attack.kill'] % enemy_name)
+                world_enemies.pop(enemy_uuid)
+                rooms[x_pos][y_pos] = [0, '']
+            else:
+                print(dialogue['debug.enemyAttack'] % (enemy_name, enemy_strength))
+                if not debug_immortal:
+                    char_health -= (enemy_strength - char_defense)
+                    print(dialogue['debug.health'] % char_health)
+                else:
+                    print(dialogue['debug.immortal'])
+
+        else:
+            print(dialogue['attack.noWeapon'] % weapon)
+    else:
+        print(dialogue['attack.noTarget'])
+    return
+
+
+all_cmd = {'walk': walk,
+           'quit': game_quit,
+           'start': game_start,
+           'exit': game_exit,
+           'options': options,
+           'attack': attack}
 # initialize
 load_cmd_sets()
 load_dialogue()
 # ready Load menu
 load_menu()  # Loads main menu
+# sets initial variable values
+world_items = {}
+x_pos = 0
+y_pos = 0
+rooms = {0: {0: [0, '']}}
+world_enemies = {}
+inventory = {'sword': 1}
+char_health = 20
+char_defense = 1
+debug_immortal = True
 quit_interpreter = False
 cmd_interpreter()
